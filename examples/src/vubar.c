4a64af9079f2c80ef7cb8ca93877531bb9ed21f2
Luan Luiz Gonçalves <luanlg.cco@gmail.com>
2020-05-09 15:11:58 -0300

    Create a method to add widget in gui component.

--- examples/src/vubar.c
+++ examples/src/vubar.c
@@ -1,6 +1,6 @@
+#include <gtk/gtk.h>
 #include <mosaic-sound.h>
 #include <portaudio.h>
-#include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -13,10 +13,10 @@ mscsound_vubar_t *vubar;
 mscsound_speaker_t *speaker;
 
 static int mscsound_callback(const void *inputBuffer, void *outputBuffer,
-                                unsigned long framesPerBuffer,
-                                const PaStreamCallbackTimeInfo *timeInfo,
-                                PaStreamCallbackFlags statusFlags,
-                                void *userData) {
+                             unsigned long framesPerBuffer,
+                             const PaStreamCallbackTimeInfo *timeInfo,
+                             PaStreamCallbackFlags statusFlags,
+                             void *userData) {
   float *in = (float *)inputBuffer;
   float *out = (float *)outputBuffer;
 
@@ -37,22 +37,20 @@ static int mscsound_callback(const void *inputBuffer, void *outputBuffer,
  * This routine is called by mscsound when mscsound_callback is done.
  */
 
- void destroy(void){
-    gtk_main_quit ();
- }
+void destroy(void) { gtk_main_quit(); }
 
 static void mscsound_finished(void *data) { printf("Stream Completed!\n"); }
 
 /*******************************************************************/
 int main(int argc, char *argv[]) {
-  mscsound_gui_t * gui = mscsound_create_gui("VUbar", 200, 400);
+  mscsound_gui_t *gui = mscsound_create_gui("VUbar", 200, 400);
 
   pb = mscsound_create_playback("../samples/victor_wooten_solo.wav",
-                                   FRAMES_PER_BUFFER);
+                                FRAMES_PER_BUFFER);
   strcpy(*(pb->loop), "yes");
 
-  vubar = mscsound_create_vubar(FRAMES_PER_BUFFER, gui->widget);
-
+  vubar = mscsound_create_vubar(FRAMES_PER_BUFFER);
+  gui->add(&gui, &(vubar->widget), FALSE, TRUE, 20);
   speaker = mscsound_create_speaker(FRAMES_PER_BUFFER);
 
   vubar->input0 = pb->output0;
