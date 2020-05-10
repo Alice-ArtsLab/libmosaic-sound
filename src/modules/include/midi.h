e27e0acd4cfeeb87d65cb7b500b679e184d91631
Luan Luiz Gonçalves <luanlg.cco@gmail.com>
2020-05-10 02:05:33 -0300

    Add midi

---
+++ src/modules/include/midi.h
@@ -0,0 +1,20 @@
+#ifndef MSCSOUND_MIDI_H
+#define MSCSOUND_MIDI_H
+
+typedef struct {
+  void *handle;
+  void (*callback)();
+  void (*send_note)(void *self, int type, int channel, unsigned char vel,
+                    unsigned char note);
+  void (*send_control)(void *self, int channel, int control, int value);
+
+} mscsound_midi_t;
+
+mscsound_midi_t *mscsound_create_midi();
+
+void mscsound_midi_send_note(void *self, int type, int channel,
+                             unsigned char vel, unsigned char note);
+void mscsound_midi_send_control(void *self, int channel, int control,
+                                int value);
+void *mscsound_midi_thread(void *data);
+#endif /* midi.h */
